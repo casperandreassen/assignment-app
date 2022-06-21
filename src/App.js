acdd5f6631f18da40d10951577aa47e3350050c0
Casper <c@csandreassen.no>
2022-06-21 12:22:45 +0200

    Started work on homepage and assignment components. Start implementing sorting of assignment list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [assignment, setAssignment] = useState([
+    {
+      course: "TDT4100",
+      assignmentName: "Øving 2",
+      dueDate: "Sun Jun 19 2022",
+      dateCreated: "Tue Jun 21 2022",
+      timeDue: "22:00",
+      gitHubLink: "random link",
+      description: "some random long description",
+      delivered: false,
+      color: "#00B76A",
+      id: "1",
+    },
+    {
+      course: "IT2805",
+      assignmentName: "Øving 1",
+      dueDate: "Tue Jun 21 2022",
+      dateCreated: "Mon Jun 20 2022",
+      timeDue: "23:59",
+      gitHubLink: "random link",
+      description: "some random long description",
+      delivered: true,
+      color: "#00B76A",
+      id: "2",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="assignments-view-container">
+      <div className="assignments-view-header">
+        <h1>Upcoming assignments</h1>
+        {/* Add sort icon here */}
+        <button
+          type="text"
+          className="sort-by-button"
+          onClick={(event) => {
+            if (event.currentTarget.textContent === "Due date") {
+              event.currentTarget.textContent = "Date created";
+              assignment.sort((a, b) => {
+                return new Date(a.dateCreated) < new Date(b.dateCreated)
+                  ? 1
+                  : -1;
+              });
+            } else {
+              event.currentTarget.textContent = "Due date";
+              assignment.sort((a, b) => {
+                return new Date(a.dueDate) < new Date(b.dueDate) ? 1 : -1;
+              });
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Due date
+        </button>
+      </div>
+      <div className="assignments-view">
+        {assignment.map((assignment) => {
+          return (
+            <div className="assignemt-container" key={assignment.id}>
+              <h2>{assignment.course}</h2>
+              <p>{assignment.assignmentName}</p>
+              <p>{assignment.dueDate}</p>
+              <p>{assignment.timeDue}</p>
+              <p>{assignment.color}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
